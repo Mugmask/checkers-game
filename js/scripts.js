9e8882d7b0fab6428ee377eeee8a8dfef4fb7a81
Francisco Perez Ugalde <franciscoperezugalde@gmail.com>
2023-02-24 03:46:44 -0300

    posibles moves

--- js/scripts.js
+++ js/scripts.js
@@ -11,24 +11,37 @@ function placeTokenToBoard() {
       if (row <= 2) {
         const token = document.createElement("div");
         token.className = "token dark";
+        token.id = "token-" + row + column;
+        token.onclick = showPossibleMove.bind(this, token, row, column);
         if (
           checkersBoard.children[row].children[column].className.includes(
             "active"
           )
         ) {
           checkersBoard.children[row].children[column].appendChild(token);
-        } else if (row >= 5) {
-          const token = document.createElement("div");
-          token.className = "token ligth";
-          if (
-            checkersBoard.children[row].children[column].className.includes(
-              "active"
-            )
-          ) {
-            checkersBoard.children[row].children[column].appendChild(token);
-          }
+        }
+      } else if (row >= 5) {
+        const token = document.createElement("div");
+        token.className = "token ligth";
+        if (
+          checkersBoard.children[row].children[column].className.includes(
+            "active"
+          )
+        ) {
+          checkersBoard.children[row].children[column].appendChild(token);
         }
       }
     }
   }
+}
+
+function showPossibleMove(token, row, column) {
+  const posiblesMoves = [
+    checkersBoard.children[row + 1].children[column + 1],
+    checkersBoard.children[row + 1].children[column - 1],
+  ];
+
+  posiblesMoves.forEach((x) => {
+    if (x) x.style.backgroundColor = "red";
+  });
 }
